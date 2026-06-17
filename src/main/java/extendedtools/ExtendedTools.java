95e69c8e16cade3cb5629520e449293d2abaff13
aurilisdev <aurilisdev@gmail.com>
2026-06-17 14:54:58 +0200

    Clean

--- src/main/java/extendedtools/ExtendedTools.java
+++ src/main/java/extendedtools/ExtendedTools.java
@@ -18,32 +18,33 @@ import net.minecraftforge.registries.RegistryObject;
 @EventBusSubscriber(modid = References.ID, bus = Bus.MOD)
 public class ExtendedTools {
 
-  public ExtendedTools() {
-    IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-    DeferredRegisters.ITEMS.register(bus);
-    DeferredRegisters.CREATIVE_TABS.register(bus);
-  }
-
-  @SubscribeEvent
-  @OnlyIn(Dist.CLIENT)
-  public static void onClientSetup(FMLClientSetupEvent event) {}
-
-  @SubscribeEvent
-  public static void onLoadEvent(FMLLoadCompleteEvent event) {}
-
-  @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = References.ID,
-      bus = Mod.EventBusSubscriber.Bus.MOD)
-  private static class ElectroCreativeRegistry {
+    public ExtendedTools() {
+	IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+	DeferredRegisters.ITEMS.register(bus);
+	DeferredRegisters.CREATIVE_TABS.register(bus);
+    }
 
     @SubscribeEvent
-    public static void registerItems(BuildCreativeModeTabContentsEvent event) {
-      CreativeModeTab tab = ((RegistryObject<CreativeModeTab>) DeferredRegisters.CREATIVE_TABS
-          .getEntries().toArray()[0]).get();
-      if (event.getTab() == tab) {
-        DeferredRegisters.ITEMS.getEntries().forEach(reg -> {
-          event.accept(reg.get());
-        });
-      }
+    @OnlyIn(Dist.CLIENT)
+    public static void onClientSetup(FMLClientSetupEvent event) {
     }
-  }
+
+    @SubscribeEvent
+    public static void onLoadEvent(FMLLoadCompleteEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = References.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    private static class ElectroCreativeRegistry {
+
+	@SubscribeEvent
+	public static void registerItems(BuildCreativeModeTabContentsEvent event) {
+	    CreativeModeTab tab = ((RegistryObject<CreativeModeTab>) DeferredRegisters.CREATIVE_TABS.getEntries()
+		    .toArray()[0]).get();
+	    if (event.getTab() == tab) {
+		DeferredRegisters.ITEMS.getEntries().forEach(reg -> {
+		    event.accept(reg.get());
+		});
+	    }
+	}
+    }
 }
